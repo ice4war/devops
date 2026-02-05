0e1d1acf44476b5806ccf76505b4457a103132ab
ice4war <bharathece26@gmail.com>
2026-02-05 22:12:56 +0530

    grafana monitoring

---
+++ docker/observability/install_observability.sh
@@ -0,0 +1,5 @@
+#grafana
+curl -O https://dl.grafana.com/grafana/release/12.3.2/grafana_12.3.2_21390657659_linux_amd64.tar.gz
+
+#influx db
+curl -O https://dl.influxdata.com/influxdb/releases/v2.8.0/influxdb2-2.8.0_linux_amd64.tar.gz
